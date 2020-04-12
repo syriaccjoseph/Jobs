628da245f2c4bffc29eedafa21ab1bfe9606116a
Ajith Joseph <syriaccjoseph@gmail.com>
2020-04-12 10:07:43 -0400

    simple project creation view

--- project/models.py
+++ project/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.forms import ModelForm
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['name']
--- project/views.py
+++ project/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+from .models import Project, ProjectForm
+
+def index(request):
+    projects_list = Project.objects.all()
+    context = {
+        'projects_list': projects_list,
+    }
+    template = loader.get_template('project/index.html')
+    return HttpResponse(template.render(context, request))
+
+def save(request):
+    project = ProjectForm(request.POST)
+    project.save()
+    return HttpResponseRedirect('/')
+    
